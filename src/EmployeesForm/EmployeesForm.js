642ac1ec3f81be447187fd13f1b2bdd47de5a665
Dixit Kachhadiya <dixitkachhadiya21@gmail.com>
2024-07-11 16:58:03 +0530

    first commit

--- src/EmployeesForm/EmployeesForm.js
+++ src/EmployeesForm/EmployeesForm.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from 'react-bootstrap';
+import axios from 'axios';
 
-function EmployeesForm(){
-    return(
+function EmployeesForm() {
+
+    const [getData, setgetData] = useState([]);
+
+    useEffect(() => {
+        getEmployeeData();
+    }, [])
+    const getEmployeeData = async () => {
+        try {
+            var res = await axios.get("https://employees-api-eight.vercel.app/getEmployeesDetails");
+            setgetData(res.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
         <>
-            <h1>Hello</h1>
+
+            <div className="table-responsive">
+                <Table striped bordered hover className="custom-table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Salary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            getData.map((item) =>
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.Name}</td>
+                                    <td>{item.LastName}</td>
+                                    <td>{item.Salary}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </>
     )
 }
